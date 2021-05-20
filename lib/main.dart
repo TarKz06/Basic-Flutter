07d9cf3a47b8d3119741ef4654836bfa7d23b762
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-20 14:55:09 +0700

    post code

--- lib/main.dart
+++ lib/main.dart
@@ -9,22 +9,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My app",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello flutter"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Hello tar"),
-              Text("Hello Ice"),
-              Text("Hello Tle")
-            ],
-          ),
-        ),
-      ),
+      home: MyHomePage(),
       theme: ThemeData(primaryColor: Colors.orange[200]),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int number = 0; //crate state
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Count number"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              number.toString(),
+              style: TextStyle(fontSize: 60),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addnumBer,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void addnumBer() {
+    setState(() {
+      number++;
+    });
   }
 }
