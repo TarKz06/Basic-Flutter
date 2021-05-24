f185c12e0fdd0b5d868b046db8d538fd69ea901a
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-24 11:24:51 +0700

    List

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'FoodMenu.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,24 +22,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int number = 0; //crate state
-
+  List<FoodMenu> menu = [
+    FoodMenu("pizza", "300"),
+    FoodMenu("somtam", "90"),
+    FoodMenu("kapao", "450"),
+    FoodMenu("padtai", "200")
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Count number"),
+          title: Text("Choose manu"),
         ),
         body: ListView.builder(
-          itemCount: 15,
-          itemBuilder: (BuildContext context, int index) {
-          return ListTile(title: Text("mamu ${index+1}"));
-        }));
-  }
-
-  void addnumBer() {
-    setState(() {
-      number++;
-    });
+            itemCount: menu.length,
+            itemBuilder: (BuildContext context, int index) {
+              FoodMenu food = menu[index];
+              return ListTile(
+                title: Text(
+                  food.name,
+                  style: TextStyle(fontSize: 39),
+                ),
+                subtitle: Text("price : " + food.price + " Bath "),
+              );
+            }));
   }
 }
