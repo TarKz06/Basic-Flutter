1105e80228ccdb20df222ae024df4d0361d2da29
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-28 14:23:21 +0700

    Asynchronou

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,73 @@
 import 'package:flutter/material.dart';
 import 'MoneyBox.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async{
+  //runApp(MyApp());
+  print(loginUser());
 }
 
-int number = 0;
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "My app",
-      home: MyHomePage(),
-      theme: ThemeData(primaryColor: Colors.purple[400]),
-    );
-  }
+Future <String> loginUser() async {
+  var user = await getUserFromDatabase();
+  return user;
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+Future <String> getUserFromDatabase() {
+  return Future.delayed(Duration(seconds: 10),()=>"Khematat");
 }
+//
+//int number = 0;
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: "My app",
+//      home: MyHomePage(),
+//      theme: ThemeData(primaryColor: Colors.purple[400]),
+//    );
+//  }
+//}
+//
+//class MyHomePage extends StatefulWidget {
+//  @override
+//  _MyHomePageState createState() => _MyHomePageState();
+//}
+//
+//class _MyHomePageState extends State<MyHomePage> {
+//  @override
+//  void initState() {
+//    super.initState();
+//    print("initstate");
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text("My Account",
+//            style: TextStyle(
+//                fontSize: 25,
+//                color: Colors.white,
+//                fontWeight: FontWeight.bold)),
+//      ),
+//      body: Column(
+//        children: [
+//          Text(
+//            number.toString(),
+//            style: TextStyle(fontSize: 30),
+//          )
+//        ],
+//      ),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: () {
+//          setState(() {
+//            number++;
+//          });
+//        },
+//        child: Icon(Icons.add),
+//      ),
+//    );
+//  }
+//}
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    print("initstate");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Account",
-            style: TextStyle(
-                fontSize: 25,
-                color: Colors.white,
-                fontWeight: FontWeight.bold)),
-      ),
-      body: Column(
-        children: [
-          Text(
-            number.toString(),
-            style: TextStyle(fontSize: 30),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            number++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+// asynchronou
